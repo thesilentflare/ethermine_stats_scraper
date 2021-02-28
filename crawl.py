3e3dae534c2179d81df39419d716c7d783ae2a41
Gavin Leung <gawainl26@gmail.com>
2021-02-28 01:57:20 -0500

    Update crawl.py
    fixed incorrect data type

--- crawl.py
+++ crawl.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from colorama import Fore, Back, Style
 from dotenv import dotenv_values
 from selenium.webdriver.chrome.options import Options
+from decimal import *
 import time
 import datetime
 import sys
@@ -15,7 +16,9 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 driver.implicitly_wait(10)
 hour_bal_time = 0
-hour_bal_gain = 0
+prev_bal = 0
+cur_bal = 0
+hour_bal_gain = 0.0
 setup = True
 time.sleep(3)
 while True:
@@ -25,28 +28,32 @@ while True:
         '//*[@id="app"]/main/div/div[2]/div[1]/div[3]/div[1]/div/div[2]/div/div[2]/span[1]').text
     curr = driver.find_element_by_xpath(
         '//*[@id="app"]/main/div/div[2]/div[1]/div[3]/div[1]/div/div[1]/div/div[2]/span[1]').text
+    reported = driver.find_element_by_xpath(
+        '//*[@id="app"]/main/div/div[2]/div[1]/div[3]/div[1]/div/div[3]/div/div[2]/span[1]').text
     valid = driver.find_element_by_xpath(
         '//*[@id="app"]/main/div/div[2]/div[1]/div[3]/div[2]/div/div[1]/div/div[2]/span').text
     stale = driver.find_element_by_xpath(
         '//*[@id="app"]/main/div/div[2]/div[1]/div[3]/div[2]/div/div[2]/div/div[2]/span').text
     rejected = driver.find_element_by_xpath(
         '//*[@id="app"]/main/div/div[2]/div[1]/div[3]/div[2]/div/div[3]/div/div[2]/span').text
-    if setup:
-        hour_bal_gain = float(unpaid)
-        setup = False
+    cur_bal = Decimal(unpaid)
     x = datetime.datetime.now()
+    if setup:
+        prev_bal = Decimal(unpaid)
+        hour_bal_time = x.hour
+        setup = False
     if (hour_bal_time != x.hour):
         hour_bal_time = x.hour
-        if (float(unpaid) != 0):
-            hour_bal_gain = float(unpaid) - hour_bal_gain
+        if (cur_bal > prev_bal):
+            hour_bal_gain = cur_bal - prev_bal
+            prev_bal = cur_bal
         else:
-            hour_bal_gain = unpaid
-
+            hour_bal_gain = 0.0
     print(Fore.WHITE + "Unpaid: " + Fore.YELLOW + "{}".format(unpaid) + Fore.WHITE + " ETH" + Fore.CYAN + " @{}".format(x) +
-          Fore.MAGENTA + " AVG@{}MH/S".format(average) + Fore.WHITE + " ; " + Fore.MAGENTA + "CUR@{}MH/S".format(curr))
+          Fore.WHITE + " |" + Fore.MAGENTA + " AVG@{}MH/S".format(average) + Fore.WHITE + " ; " + Fore.MAGENTA + "CUR@{}MH/S".format(curr) + Fore.WHITE + " ; " + Fore.MAGENTA + "REP@{}MH/S".format(reported) + Fore.WHITE + " |")
     print(Fore.GREEN + "Valid: {}".format(valid) + Fore.WHITE + " ; " + Fore.YELLOW +
           "Stale: {}".format(stale) + Fore.WHITE + " ; " + Fore.RED + "Rejected: {}".format(rejected))
     print(Fore.WHITE + "Hourly Gain: " + Fore.GREEN + " {}".format(hour_bal_gain) +
           Fore.WHITE + " ETH; Last Updated" + Fore.CYAN + " @{}:00hrs".format(hour_bal_time))
     driver.refresh()
-    time.sleep(10)
+    time.sleep(30)
